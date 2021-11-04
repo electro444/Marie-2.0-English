4d91e1b1dc06b4bc5234b28645c28822997726f9
electro444 <93459621+electro444@users.noreply.github.com>
2021-11-04 20:37:43 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,19 +18,15 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-hoi {}, my name is {}! if you have any questions about how to use me please give me /help... 
+Hi I'm *God Of Pikachu*!
 
-im a group manager bot maintained by  [this person](tg://user?id={}).
+I'm Help To Manage Your Groups And Im Very Powerfull Bot!
 
-My future updates will be put into This Channel - @MarieChechi & My Support Group @InFoTelGroup.
+Click /help below to find out more about how to use me to to my full potential.
 
-This is my [Deploy Code](https://heroku.com/deploy?template=https://github.com/TGExplore/Marie-2.0-English),
-you can create clone same like me..
+Click and [ADD ME YOUR GROUP](https://t.me/God_Of_Pikachu_Bot?startgroup=true)
 
-For more commands click /help...
-
-**Keep in mind that any changes you DO do to the source have to be on github, as per the license.**
-
+[Bot Owner](tg://user?id={}).
 """
 
 HELP_STRINGS = """
